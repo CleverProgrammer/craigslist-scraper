13a77b6c13f69f0be297c94bb5de723f70d751f4
Rafeh Qazi <rafehqazi@Rafehs-MBP.attlocal.net>
2019-08-03 16:19:40 -0700

    Final add images to frontend.

--- my_app/views.py
+++ my_app/views.py
@@ -30,7 +30,6 @@ def new_search(request):
         min_price = request.POST.get('min_price', 0)
         max_price = request.POST.get('max_price', 1000)
         final_url = url.format(quote_plus(search), min_price, max_price)
-        print(final_url)
 
         # Getting the webpage, creating a Response object.
         response = requests.get(final_url)
@@ -41,35 +40,27 @@ def new_search(request):
         soup = BeautifulSoup(data, features='html.parser')
         # Extracting all the <a> tags whose class name is 'result-title' into a list.
         titles = soup.findAll('a', {'class': 'result-title'})
-        print(titles[0].get('href'))
+
+        images = soup.find_all("a", {"class": "result-image"})
+
+        image_url = "https://images.craigslist.org/{}_300x300.jpg"
+
+        final_image_urls = []
+        for image in images:
+            if image.get('data-ids'):
+                # append the image URL to final images list
+                final_image_urls.append(image_url.format((image.get('data-ids').split(',')[0].split(':')[1])))
+            else:
+                # append a "NOT AVAILABLE" image URL to final images list
+                final_image_urls.append('https://craigslist.org/images/peace.jpg')
+
+        print(final_image_urls)
+
         stuff_for_frontend = {
             'titles': [(title.text, title.get('href')) for title in titles],
             'search': search,
+            'final_image_urls': final_image_urls,
         }
-        print(stuff_for_frontend)
-        # Extracting text from the the <a> tags, i.e. class titles.
-        for title in titles:
-            print(title.text)
-
-        rowArray = soup.find_all("li", {"class": "result-row"})
-        print(rowArray[0].find('a', class_='result-image gallery'))
-        # test_images = soup.find_all("img", {"class": "swipe"})
-        test_images = soup.find_all("a", {"class": "result-image"})
-        image_id = test_images[0].get('data-ids').split(',')[0].split(':')[1]
-        final_image_url = "https://images.craigslist.org/{}_300x300.jpg".format(image_id)
-        print(final_image_url)
-        # print(soup)
-        # print(rowArray[0])
-        # print(rowArray[0].find('div', class_='swipe-wrap'))
-        # print(rowArray)
-
-        for row in rowArray:
-            img = row.find("img")
-            if img is None:
-                continue
-            if isinstance(img, Tag) and img.has_attr("src"):
-                print(img['src'])
-                print("----------------")
 
         return render(request, 'my_app/new_search.html', stuff_for_frontend)
     return render(request, 'my_app/new_search.html')
