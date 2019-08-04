b7a2116f35cb2a03a3702337a5ec58bdcb9edad6
Rafeh Qazi <rafehqazi@Rafehs-MBP.attlocal.net>
2019-08-03 17:55:56 -0700

    Card functionality is working perfectly now. It is now more robust and everything gets iterated over at the same time.

--- my_app/views.py
+++ my_app/views.py
@@ -39,7 +39,57 @@ def new_search(request):
         # Passing the source code to Beautiful Soup to create a BeautifulSoup object for it.
         soup = BeautifulSoup(data, features='html.parser')
         # Extracting all the <a> tags whose class name is 'result-title' into a list.
+        post_listings = soup.find_all('li', {'class': 'result-row'})
+        # print(post_listings, 'FINAL RESULTS LIST TEST!!!')
+        # print(test_final_list[0].text)
+        # print(post_listings[0].find('a').get('href'))
+        # print(post_listings[0].find(class_='result-price').text)
+        # print(post_listings[0].find(class_='result-title').text)
+        # print(post_listings[0].find(class_='result-image').get('data-ids'))
+
+        # post_title = post_listings[0].find(class_='result-title').text
+        # post_url = post_listings[0].find('a').get('href')
+        # post_price = post_listings[0].find(class_='result-price').text
+        # post_image_id = post_listings[0].find(class_='result-image').get('data-ids')
+
+        # currently working here...
+        final_postings = []
+        for post in post_listings:
+            post_title = post.find(class_='result-title').text
+            post_url = post.find('a').get('href')
+
+            if post.find(class_='result-price'):
+                post_price = post.find(class_='result-price').text
+            else:
+                post_price = 'N/A'
+
+
+            if post.find(class_='result-image').get('data-ids'):
+                post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+                post_image_url = "https://images.craigslist.org/{}_300x300.jpg".format(post_image_id)
+            else:
+                post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+            final_postings.append((post_title, post_url, post_price, post_image_url))
+
+        print(final_postings, 'FINAL POSTINGS WOAH!!!')
+        print(len(final_postings), 'that is the length!!!')
+        print(final_postings[9], '9th item')
+
+        # Need to build a post image URL.
+
         titles = soup.findAll('a', {'class': 'result-title'})
+
+        prices = soup.findAll('span', {'class': 'result-price'})
+
+        final_prices = []
+        for price in prices:
+            if price:
+                final_prices.append(price.text)
+            else:
+                final_prices.append('N/A')
+
+        print(final_prices, 'PRICES WOOHOO!!!!')
 
         images = soup.find_all("a", {"class": "result-image"})
 
@@ -56,10 +106,16 @@ def new_search(request):
 
         print(final_image_urls)
 
+        title_stuff = []
+        for title, url, price in zip(titles, final_image_urls, final_prices):
+            title_stuff.append((title.text, title.get('href'), url, price))
+
+        print(title_stuff, '<--- STUFF!')
+
         stuff_for_frontend = {
-            'titles': [(title.text, title.get('href')) for title in titles],
+            'title_stuff': title_stuff,
             'search': search,
-            'final_image_urls': final_image_urls,
+            'final_postings': final_postings,
         }
 
         return render(request, 'my_app/new_search.html', stuff_for_frontend)
